1e631f4b10b60290bd2a13a9456a30cef80e8115
rain <rheigneabril27@gmail.com>
2025-11-29 18:29:56 +0800

    Update GamePhase.java
    removed unused code blocks

--- moonlightsgambit/src/interfaces/GamePhase.java
+++ moonlightsgambit/src/interfaces/GamePhase.java
@@ -7,17 +7,7 @@ public interface GamePhase {
     
     // Executes specific phase logic
     void executePhase(MoonlightsGambit game);
-    
     // Gets display name of phase
     String getPhaseName();
-    
-    // Optional cleanup logic
-    default void cleanupPhase(MoonlightsGambit game) {
-        // Default empty implementation
-    }
-    
-    // Validates if phase can execute
-    default boolean canExecute(MoonlightsGambit game) {
-        return game != null;
-    }
 }
+
